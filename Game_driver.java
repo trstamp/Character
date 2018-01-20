a7397d53f0566117f2186cb5be2896d9099b0068
trstamp <35055803+trstamp@users.noreply.github.com>
2018-01-19 22:31:36 -0800

    Printwriter stuff
    Defined the printwriter class, gave a name to the text file that stores all the character names, created the options for a new character and existing character.

--- Game_driver.java
+++ Game_driver.java
@@ -1,5 +1,6 @@
 package d141;
 import java.util.Scanner;
+import java.io.*;
 
 public class Game_driver {
 	
@@ -7,12 +8,43 @@ public class Game_driver {
 	static Character a = new Character();
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
+		Printwriter outputFile = new Printwriter("Characters.txt");
 	System.out.println("Hello, Welcome to the Game\nIf you have a Character already made press 1\nIf you want to make a new Character press 2");
+	int x = sc.nextInt();
 	//this is the printwriter option should go
-	
+	switch(x) 
+	{
+		case 1:
+		{
+			System.out.print("Please enter your name: ");
+			String name = sc.nextLine();
+			
+			Scanner file = new Scanner(outputFile);
+			String n = file.nextLine();
+			
+			if(n.equalsIgnoreCase(name))
+			{
+				System.out.println("Welcome back " + name + ", here are your stats: ");
+				System.out.println("Gender : " + name.getGender());
+				System.out.println("Armor : " + name.getArmor());
+				System.out.println("Weapon : " + name.getWeapon());
+				System.out.println("Agility : " + name.getAgility());
+				System.out.println("Defense : " + name.getDefense());
+			}
+			break;
+		}
 	// new character
-	
+		case 2:
+		{
+			System.out.print("Please enter your name: ");
+			String name = sc.nextLine();
+			outputFile.println(name);
+			outputFile.close();
+			break;
+		}
+		default
+			System.out.println("Invalid option\nIf you have a Character already made press 1\nIf you want to make a new Character press 2");
+	}
 	System.out.println("Are you a male or female : ");
 	String gender = sc.next();
 	a.setGender(gender);
